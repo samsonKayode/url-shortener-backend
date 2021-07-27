ada20652cc7e821ffc1f08c08d25629f98c6c300
kayode <samsonkayode@gmail.com>
2021-07-27 18:55:04 +0100

    Completed mock tests for UrlService implementation

--- src/test/java/com/url/shortner/backend/service/UrlServiceTest.java
+++ src/test/java/com/url/shortner/backend/service/UrlServiceTest.java
@@ -1,14 +1,17 @@
 package com.url.shortner.backend.service;
 
 import com.url.shortner.backend.dto.UrlDto;
+import com.url.shortner.backend.entity.Url;
 import com.url.shortner.backend.repository.IUrlRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class UrlServiceTest {
@@ -16,6 +19,8 @@ class UrlServiceTest {
     @Mock
     private IUrlRepository repository;
     private UrlService underTest;
+
+    private String hashedURL="1dd1ac64";
 
     @BeforeEach
     void setUp() {
@@ -25,25 +30,41 @@ class UrlServiceTest {
     @Test
     void convertUrl() {
 
-        String hashedFacebookURL="1dd1ac64";
         //when
         UrlDto urlDto = new UrlDto();
         urlDto.setLongUrl("http://facebook.com");
         String returnedURL = underTest.convertUrl(urlDto);
 
-        //then.
-        assertThat(returnedURL).contains(hashedFacebookURL);
+        ArgumentCaptor<Url> urlArgumentCaptor = ArgumentCaptor.forClass(Url.class);
+        verify(repository).save(urlArgumentCaptor.capture());
+
+        Url URL = urlArgumentCaptor.getValue();
+        //then
+        assertThat(returnedURL).contains(URL.getHashUrl());
+
     }
 
     @Test
     void decodeUrl() {
+        //when
+        repository.findByHashUrl(hashedURL);
+        //then
+        verify(repository).findByHashUrl(hashedURL);
     }
 
     @Test
     void getShortUrlStatistics() {
+        //when
+        repository.findByHashUrl(hashedURL);
+        //then
+        verify(repository).findByHashUrl(hashedURL);
     }
 
     @Test
     void getAllUrlList() {
+        //when
+        repository.findAll();
+        //then
+        verify(repository).findAll();
     }
 }
